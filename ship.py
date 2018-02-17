cb38a349f5eee26132dc62ce0b1ebfb15e3cfcd9
William Horn <wbhorn@alaska.edu>
2018-02-16 17:38:10 -0900

    Make players have the right type of ships.

--- ship.py
+++ ship.py
@@ -3,3 +3,19 @@ class Ship:
         self.length = length
 
 
+def get_init_ships():
+
+    ship_types = [
+        [5, 1],
+        [4, 2],
+        [3, 3],
+        [3, 4],
+        [2, 5]
+    ]
+
+    ships = []
+    for length, amount in ship_types:
+        len_sized_ships = [Ship(length) for _ in range(amount)]
+        ships += len_sized_ships
+
+    return ships
--- test_battle_ship.py
+++ test_battle_ship.py
@@ -1,10 +1,11 @@
 import unittest
-from player import Player, get_init_player_ships
+from ship import get_init_ships
+from player import Player
 
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
-        init_ships = get_init_player_ships()
+        init_ships = get_init_ships()
         self.player = Player(init_ships)
 
     def test_players_have_pieces(self):
