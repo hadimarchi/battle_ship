ba26d1396ce390d7209830822b7bcd21c0673259
William Horn <wbhorn@alaska.edu>
2018-02-16 17:30:14 -0900

    Test players have ships of correct size.

--- test_battle_ship.py
+++ test_battle_ship.py
@@ -11,6 +11,12 @@ class TestPlayer(unittest.TestCase):
         num_ships = len(self.player.ships)
         self.assertEqual(num_ships, 15)
 
+    def test_players_have_right_kinds_of_ships(self):
+        ship_sizes = set(range(5, 1, -1))
+
+        for ship in self.player.ships:
+            self.assertIn(ship.length, ship_sizes)
+
 
 if __name__ == '__main__':
     unittest.main()
