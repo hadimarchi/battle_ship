d744575e8a54d85b65c79d57519e6404abdf7129
hadimarchi <hal.dimarchi55@gmail.com>
2018-02-17 15:26:36 -0900

    fixed placement logic

---
+++ game.py
@@ -0,0 +1,8 @@
+from battle_ship.ship import Ship
+from battle_ship.player import Player
+
+
+class Game:
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
